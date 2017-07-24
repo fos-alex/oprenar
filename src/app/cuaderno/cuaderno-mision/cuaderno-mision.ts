5948e72f650f250f277111c2b4a1ae3049791ea2
Alex Otero <fos.alex@gmail.com>
2017-07-23 23:24:16 -0300

    SVG Cuaderno con id

--- src/app/cuaderno/cuaderno-mision/cuaderno-mision.ts
+++ src/app/cuaderno/cuaderno-mision/cuaderno-mision.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
     selector: 'cuaderno-mision'
 })
 export class CuadernoMision {
+    constructor(private renderer: Renderer2,
+                private elRef:ElementRef,
+                private router: Router) {
+
+    }
+
+    switchPage(page) {
+        this.router.navigate(['/cuaderno', page]);
+    }
+
 }
